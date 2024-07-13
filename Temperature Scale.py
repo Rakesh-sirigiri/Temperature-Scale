c22c985ab70f4c7ae0289c059cfe25bff0434eb3
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-07-13 20:27:19 +0530

    Add files via upload

---
+++ Temperature Scale.py
@@ -0,0 +1,7 @@
+a = int(input("enter the temperature in degree "))
+f = a*(8/5)+32
+print("Temperature in Fahrenhiet =",f)
+
+m = int(input("enter the temperature in fahrenhiet "))
+c = (m-32)+(5/8)
+print("Temperature in Fahrenheit=",c)
